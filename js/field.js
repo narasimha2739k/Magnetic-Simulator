ad8d61bd52e9b0a1882fb38549741836f5cdd6cf
Narasimha Rao Kosanam <narasimharaokosanam093@gmail.com>
2026-06-26 21:09:25 +0530

    Milestone 2: Draw magnetic field lines around wire

--- js/field.js
+++ js/field.js
@@ -9,6 +9,7 @@ function drawWire() {
 
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawFieldLines();
 
     // Find the center of the canvas
     const centerX = canvas.width / 2;
@@ -19,8 +20,27 @@ function drawWire() {
     ctx.arc(centerX, centerY, 15, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
+    
 
 }
 
 // Draw once
 drawWire();
+function drawFieldLines() {
+
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    // Draw 6 concentric circles
+    for (let radius = 40; radius <= 180; radius += 25) {
+
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+
+        ctx.strokeStyle = "#2196F3";
+        ctx.lineWidth = 2;
+
+        ctx.stroke();
+    }
+
+}
